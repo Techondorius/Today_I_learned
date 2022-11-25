c85c0ae65ed84acc3d1002cd6e26f4ae37eb1c63
Kyosuke Fujita <kurofuku01@gmail.com>
2022-11-25 15:14:58 +0900

    vue学習継続

---
+++ vue/first_vue/src/App_ex3.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  data() {
+    return {
+      message: "Mmmm..."
+    }
+  },
+  mounted() {
+    this.$refs.p.textContent = "mounted"
+  }
+}
+</script>
+
+<template>
+  <p ref="p">Mounting</p>
+</template>
---
+++ vue/first_vue/src/main.js
@@ -0,0 +1,10 @@
+import { createApp } from 'vue'
+import App from './App_ex1.vue'
+import App2 from './App_ex2.vue'
+import App3 from './App_ex3.vue'
+import App4 from './App_ex4.vue'
+
+createApp(App).mount('#app')
+createApp(App2).mount('#app2')
+createApp(App3).mount('#app3')
+createApp(App4).mount('#app4')
