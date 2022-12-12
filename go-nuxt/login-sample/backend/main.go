69d8130c1ba6b4cb1328d79c30803f7509cb709b
Kyosuke Fujita <kurofuku01@gmail.com>
2022-12-12 11:16:31 +0900

    JWTの検証ができるようになった

--- go-nuxt/login-sample/backend/main.go
+++ go-nuxt/login-sample/backend/main.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"time"
+
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v4"
 )
 
 func main() {
@@ -11,5 +15,38 @@ func main() {
 	g.GET("/", func(c *gin.Context) {
 		c.JSON(200, "obj any")
 	})
+	g.GET("/getJWT", GetJWT)
+	g.GET("/checkJWT", CheckJWT)
 	g.Run()
+}
+
+func GetJWT(c *gin.Context) {
+	claims := jwt.MapClaims{
+		"user_id": 123456788,
+		"exp":     time.Now().Add(time.Hour * 24).Unix(),
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, _ := token.SignedString([]byte("Miteh"))
+	log.Println(tokenString)
+	c.JSON(200, gin.H{
+		"token": tokenString,
+	})
+}
+
+func CheckJWT(c *gin.Context) {
+	tokenString := c.Request.Header.Get("token")
+	fmt.Println(tokenString)
+	token, _ := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			fmt.Println("err!!!")
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+
+		return []byte("Miteh"), nil
+	})
+
+	if claims, ok := token.Claims.(jwt.MapClaims); ok {
+		fmt.Println(claims["user_id"])
+		c.JSON(200, claims["user_id"])
+	}
 }
