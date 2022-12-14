04c7e629cc3b190cf9f2c397fd67f08ed475584f
Kyosuke Fujita <kurofuku01@gmail.com>
2022-12-14 15:11:59 +0900

    コンポーネントのImport方法変更など...
    JavaScriptを不許可にしました
    絶対パスでImportできるようにしたはずが出来ていなかったので、修正しました
    useRefだけでなく、useStateを試したりしました

--- go-nuxt/login-sample/frontend/pages/index.tsx
+++ go-nuxt/login-sample/frontend/pages/index.tsx
@@ -1,16 +1,46 @@
-import { useRef, useState } from 'react'
-import SubTest from '../components/SubTest'
+import React, { useRef, useState } from 'react'
+import SubTest from '@/components/SubTest'
 
 export default function Home() {
   const [msg, setMsg] = useState('asdf')
+  const [count, setCount] = useState<number>(0)
 
-  // const changeMsg = setMsg(() => {
-  // 	return "Hello";
-  // });
-  const a = 1
+  const addCount = (n: number) => {
+    setCount(count + n)
+  }
+
+  const updateMsg = (newMsg: string) => {
+    setMsg(newMsg)
+  }
+
+  const inputField = useRef<HTMLInputElement>(null)
+  const handleClick = () => {
+    if (inputField.current) {
+      updateMsg(inputField.current.value)
+      console.log(inputField.current.value)
+    }
+  }
+
   return (
     <>
+      <p>{count}</p>
+      <button
+        onClick={() => {
+          addCount(1)
+        }}
+      >
+        CountUp
+      </button>
+      <button
+        onClick={() => {
+          addCount(-1)
+        }}
+      >
+        CountDown
+      </button>
       <h1>{msg}</h1>
+      <input type='text' ref={inputField} />
+      <button onClick={handleClick}>Touch Me!</button>
       <SubTest msg={'SubTest'} />
     </>
   )
