0dc5a2db7f071cfe0053e2da849fe6e67586e5ad
Kyosuke Fujita <kurofuku01@gmail.com>
2022-12-15 15:53:56 +0900

    バックエンドロガー設置

--- go-nuxt/login-sample/backend/main.go
+++ go-nuxt/login-sample/backend/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"time"
 
@@ -13,15 +16,23 @@ import (
 func main() {
 	fmt.Println("Hello World")
 	g := gin.Default()
+
 	// Cors
 	config := cors.DefaultConfig()
 	config.AllowAllOrigins = true
+	config.AllowHeaders = []string{"token"}
 	g.Use(cors.New(config))
+
+	// logger
+	g.Use(logger())
+
 	g.GET("/", func(c *gin.Context) {
 		c.JSON(200, "obj any")
 	})
 	g.GET("/getJWT", GetJWT)
+	g.POST("/getJWT", GetJWT)
 	g.GET("/checkJWT", CheckJWT)
+	g.POST("/checkJWT", CheckJWT)
 	g.Run()
 }
 
@@ -40,7 +51,6 @@ func GetJWT(c *gin.Context) {
 
 func CheckJWT(c *gin.Context) {
 	tokenString := c.Request.Header.Get("token")
-	fmt.Println(tokenString)
 	token, _ := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
 			fmt.Println("err!!!")
@@ -53,5 +63,19 @@ func CheckJWT(c *gin.Context) {
 	if claims, ok := token.Claims.(jwt.MapClaims); ok {
 		fmt.Println(claims["user_id"])
 		c.JSON(200, claims["user_id"])
+	}
+}
+func logger() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		ByteBody, _ := ioutil.ReadAll(c.Request.Body)
+		c.Request.Body = ioutil.NopCloser(bytes.NewBuffer(ByteBody))
+		log.Println("Endpoint: " + c.FullPath())
+		log.Println("Body: " + string(ByteBody))
+
+		q := c.Request.URL.Query()
+		j, _ := json.Marshal(q)
+		log.Println("Query Params: " + string(j))
+
+		c.Next()
 	}
 }
